397059254b1c44607a1a1e9dd23982cdc52af113
Kwasi-Asiedu-Mensah <70768599+Kwasi-Asiedu-Mensah@users.noreply.github.com>
2023-04-21 15:37:00 +0000

    Add files via upload
    First commit

---
+++ beginner.js
@@ -0,0 +1,65 @@
+// console.log("Hello, World!"); //first hello world run
+
+
+// //variables
+
+// let age = 25; // number variable as well
+// console.log(age)
+
+// const salary = 2500
+// console.log(salary);
+
+// console.log(age + salary) //testing to see if addition will work
+
+//when const is intitalized, you cannot change the value so code below wont work because there's no const value
+
+// const sum
+// console.log(sum)
+
+//const sum = 0
+// sum = 10
+//console.log(sum) 
+//this will not work because const was initially set to zero
+
+
+
+//data types
+// const name = "shugz" // string
+// console.log(name);
+
+// use null for 0 values instead of undefined
+
+// const kwasi = {
+//     fname : 'kwasi',
+//     lname : 'asiedu-mensah',
+//     age : 21,
+// }
+// console.log(kwasi.fname)
+
+// const axon = ['esther', 'eli', 'william', 'kofi', 'eddie', 'audrey']
+// console.log(axon[2]) // array example
+
+
+
+
+
+//operators
+// (++x) - increases value by 1 (--x) - decreases value by 1
+
+//comparison operators compare two values and return a boolean
+// (==) - checks if two values are equal 
+// (!=) - checks if they are not equal
+// (===) - checks data type and value 
+// (!=) - strictly not equal to 
+// (&&) - returns true if both conditions are true
+// (||)- returns true is either operants are true
+//(+) - to join strings
+
+
+
+// equality
+//(==) - converts to same type
+// (===) - more strict and will not convert implicitly 
+
+//conditional statements
+//if, else, elseif, switch
